62ed38a0bd1adac7b86318e699d2905f74252830
FundXJim <31919672+FundXJim@users.noreply.github.com>
2021-02-12 15:47:09 +0000

    Add files via upload

---
+++ USFilingsService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text.Json;
+using FundXWebBlzr.Models;
+
+
+
+namespace FundXWebBlzr.Services
+{
+    public class USFilingsService
+    {
+        public HttpClient _httpClient;
+
+        public USFilingsService(HttpClient client)
+        {
+            _httpClient = client;
+        }
+
+
+        public async Task<List<Val>> GetValsAsync(string tkr, string tag, short rYr)
+        {
+            var response = await _httpClient.GetAsync($"api/v1/us/std/items/{tag}/fiscal/yr/{rYr}/{tkr}?tkn=52");
+            response.EnsureSuccessStatusCode();
+
+            using var responseContent = await response.Content.ReadAsStreamAsync();
+            return await JsonSerializer.DeserializeAsync<List<Val>>(responseContent);
+        }
+
+        public async Task<List<Co>> GetCosAsync(string nTry)
+        {
+            var response = await _httpClient.GetAsync($"api/v1/us/rep/cos/try/{nTry}");
+            response.EnsureSuccessStatusCode();
+
+            using var responseContent = await response.Content.ReadAsStreamAsync();
+            return await JsonSerializer.DeserializeAsync<List<Co>>(responseContent);
+        }
+
+        
+
+        public async Task<ToDo> GetToDoByIdAsync(int id)
+        {
+            var response = await _httpClient.GetAsync($"todos/{id}");
+            response.EnsureSuccessStatusCode();
+
+            using var responseContent = await response.Content.ReadAsStreamAsync();
+            return await JsonSerializer.DeserializeAsync<ToDo>(responseContent);
+        }
+    }
+}
---
+++ Val.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FundXWebBlzr.Models
+{
+
+    public class Val
+    {
+        public string Company { get; set; }
+        public string Standard_Tag { get; set; }
+        public string Tag { get; set; }
+        public int Filing_Year { get; set; }
+        public string Period_Date { get; set; }
+        public int Period_Days { get; set; }
+        public float Value { get; set; }
+    }
+
+}
