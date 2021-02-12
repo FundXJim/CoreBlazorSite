fa47ce0cd8f8efd338d2bc54dc52b03657642d8a
FundXJim <31919672+FundXJim@users.noreply.github.com>
2021-02-12 16:02:13 +0000

    Add files via upload

---
+++ Controllers.vb
@@ -0,0 +1,38 @@
+﻿Imports System.Net.Http
+Imports System.Web.Http
+Imports System.Web.HttpContext
+Imports System.Web.Http.Cors
+Imports XbrlX
+
+'This defines the endpoints.
+Namespace Controllers
+    
+    <RoutePrefix("api/v1/us/std/items")> 'This sets a default prefix for the controller. NOTE: We need the word items otherwise will read secions as tag.
+    Public Class StdValuesController
+        Inherits ApiController
+        Dim currCallType As String
+        Dim currUser As UsrLogin, currCos As Companies, currValues As Values
+        Public Sub New() 'Think you always need a parameterless constructor even if empty.
+            currUser = New UsrLogin
+            currCos = New Companies
+            currValues = New Values(currCos)
+        End Sub
+        'PeriodNo can now be specified relative now so 0, -1,-2 are all valid! You can also leave off the minus sign. Also "12" brings back 1 & 2 for a filing.
+        <Route("{tag}/fiscal/yr/{periodNo:int?}/{cos?}/{tkn:int?}")>
+        Public Function GetLatestStdYrVal(tag As String, Optional periodNo As Short = 0, Optional cos As String = "msft", Optional tkn As Integer = 0) As IHttpActionResult
+            currCallType = "LatestStdYrVal"
+            If Not Check.testParams(tkn, cos, 3000000, 1, periodNo, currCallType, tag, Nothing) Then
+                Return BadRequest(Check.msg)
+            End If
+
+            If currValues.GetLatestStdValue(cos, tag, (-1 * periodNo) + 1) Then
+                Return Ok(currValues.Values)
+            Else
+                Return BadRequest(Check.msg)
+            End If
+
+        End Function
+
+    End Class
+ 
+End Namespace
---
+++ Values.vb
@@ -0,0 +1,84 @@
+﻿Imports System.Data.SqlClient
+Imports System.Configuration.ConfigurationManager
+Imports XbrlX
+Public Class Values
+    Dim cos As Companies
+    Property Values As List(Of Value)
+    Property SECNums As List(Of SECNum)
+    Public downLoadTot As Short
+    Dim tkrTable As DataTable
+    Dim filingsConn As SqlConnection
+    Public errMsg As String
+    Public Sub New()
+    End Sub
+    Public Sub New(cs As Companies)
+        cos = cs
+    End Sub
+    Private Function openConnection() As Boolean
+        filingsConn = New SqlConnection(ConnectionStrings("FilingsDb").ConnectionString)
+        Try
+            filingsConn.Open()
+        Catch ex As Exception
+            'Pick up filingsConn open exception
+            Check.msg = Check.msg & " Db open fail " & ex.Message
+            Check.Log()
+            Return False
+        End Try
+        Return True
+    End Function
+    Sub loadTkrTable(tkrs As String) 'To be removed as now in Check.
+        tkrTable = New DataTable()
+        Dim tkrArr = Split(tkrs, ",")
+        tkrTable.Columns.Add("Ticker", GetType(String))
+        For i = 0 To tkrArr.Count - 1
+            tkrTable.Rows.Add(tkrArr(i))
+        Next
+        'These tickers may not exist in the db so could be false count and downloads get falsely reduced. If none present then
+        'no records get returned so this will get trapped and downloads will not change.
+        downLoadTot = tkrArr.Count
+    End Sub
+  
+    Public Function GetLatestStdValue(tkr As String, tag As String, rYr As Short) As Boolean
+
+        Dim dataCmd As SqlCommand, dataRdr As SqlDataReader
+
+        If Not openConnection() Then
+            Return False
+        End If
+
+
+        'Stored procedure alt.
+        'dataCmd = New SqlCommand("GetValues", filingsConn)
+        dataCmd = New SqlCommand("GetLatestStdValues", filingsConn)
+        dataCmd.CommandType = CommandType.StoredProcedure
+        dataCmd.Parameters.AddWithValue("@Tkr", tkr)
+        dataCmd.Parameters.AddWithValue("@Tag", tag)
+        dataCmd.Parameters.AddWithValue("@RelYr", rYr)
+
+        dataCmd.CommandTimeout = 1000 '600
+        Try
+            dataRdr = dataCmd.ExecuteReader()
+        Catch ex As Exception
+            Check.msg = Check.msg & " Db query fail " & ex.Message
+            Check.Log()
+            Return False
+        End Try
+
+        Values = New List(Of Value)
+
+
+        Dim linkRoot As String = AppSettings("EdgarLinkRoot")
+        Do While dataRdr.Read()
+            Values.Add(New Value(dataRdr("CoName"), dataRdr("CamelTag"), dataRdr("StdTag"), dataRdr("EndDate") / 10000, dataRdr("EndDate"), dataRdr("days"), dataRdr("Value1")))
+            'Values.Add(New Value(dataRdr(0), dataRdr(1), dataRdr(2) / 10000, dataRdr(2), dataRdr(3), dataRdr(4)))
+        Loop
+        dataRdr.Close()
+        filingsConn.Close()
+
+        Check.msg = "Ok(" & Values.Count & ")"
+        Check.Log()
+        Return True
+
+
+    End Function
+End Class
